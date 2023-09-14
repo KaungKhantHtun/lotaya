d443ae384bf45a446327c9c10f324d12c85e1843
eizin <eizin.htun@wavemoney.com.mm>
2023-09-14 10:02:43 +0630

    add onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hakathon_service/presentation/cubit/booking_cubit.dart';
 import 'package:hakathon_service/presentation/pages/booking/bookings_screen.dart';
 import 'presentation/pages/home/home_screen.dart';
+import 'presentation/pages/onboarding/onboarding_screen.dart';
 import 'services/firebase_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -48,9 +49,10 @@ class _MyAppState extends State<MyApp> {
       // home: CleaningServiceScreen(
       //   serviceProvider: serviceProviderEntity,
       // ),
-      home: const HomeScreen(
-        initialIndex: 0,
-      ),
+      // home: const HomeScreen(
+      //   initialIndex: 0,
+      // ),
+      home:OnboardingScreen(),
       // home: FreelancerScreen(),
     );
   }
